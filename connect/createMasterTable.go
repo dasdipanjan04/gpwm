023b7a4632d9a671d7d54313de4eaabfa0338804
Dipanjan Das <dasdipanjan04@gmail.com>
2020-12-16 03:06:32 +0100

    Add a simple update table feature. Needs improvement.

--- connect/createMasterTable.go
+++ connect/createMasterTable.go
@@ -10,6 +10,7 @@ import (
 
 func CreateMasterKeyTable() *sql.DB {
 	createTable := `create table if not exists mastertable (
+		id SERIAL PRIMARY KEY,
 		first_name TEXT NOT NULL,
 		last_name TEXT NOT NULL,
 		email TEXT UNIQUE NOT NULL,
@@ -42,6 +43,21 @@ func InsertMasterKeyDataToDB(db *sql.DB, first_name string, last_name string,
 	created_at := strconv.FormatInt(time_now, 10)
 	updated_at := strconv.FormatInt(time_now, 10)
 	_, err := db.Exec(insertStatement, first_name, last_name, email, master_key, created_at, updated_at, is_active)
+	if err != nil {
+		panic(err)
+	}
+	return err
+}
+
+func UpdateInfo(db *sql.DB, id int, first_name string, last_name string,
+	email string, master_key string, created_at string,
+	updated_at string, is_active bool) error {
+
+	updateStatement := `UPDATE mastertable 
+	SET first_name = $2, last_name = $3, email = $4, master_key = $5, created_at = $6, updated_at = $7, is_active = $8
+	WHERE id = $1;`
+
+	_, err := db.Exec(updateStatement, id, first_name, last_name, email, master_key, created_at, updated_at, is_active)
 	if err != nil {
 		panic(err)
 	}
--- gpass_main.go
+++ gpass_main.go
@@ -3,11 +3,20 @@ package main
 import (
 	"fmt"
 	"gpwm/connect"
+	"strconv"
+	"time"
 )
 
 func main() {
 	db := connect.CreateMasterKeyTable()
-	err := connect.InsertMasterKeyDataToDB(db, "TesFN", "TestLN", "firsttest@test.test", "Test", true)
+	err := connect.InsertMasterKeyDataToDB(db, "TesFNffgsfg", "TestLN", "firsttfsgsfgsfdest@test.test", "Test", true)
+	if err != nil {
+		panic(err)
+	}
+	time_now := time.Now().Unix()
+	created_at := strconv.FormatInt(time_now, 10)
+	updated_at := strconv.FormatInt(time_now, 10)
+	err = connect.UpdateInfo(db, 1, "TesFN", "TestLN", "test@test.test", "FooBar", created_at, updated_at, true)
 	if err != nil {
 		panic(err)
 	}
