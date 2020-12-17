8de4d6f0078152dba915296ae07cc06457bacf88
Dipanjan Das <dasdipanjan04@gmail.com>
2020-12-17 23:49:42 +0100

    WIP: Key Encryption

--- masterkeysecure/hashMasterKey.go
+++ masterkeysecure/hashMasterKey.go
@@ -1,12 +1,59 @@
 package masterkeysecure
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
 	"crypto/sha256"
-	"encoding/hex"
+	"io"
+	"log"
 )
 
-func GenerateMasterKeyHash(masterKey string) string {
-	hasherEngine := sha256.New()
-	hasherEngine.Write([]byte(masterKey))
-	return hex.EncodeToString(hasherEngine.Sum(nil))
+func GenerateMasterKeyHashSha256(masterKey string) [sha256.Size]byte {
+	return sha256.Sum256([]byte(masterKey))
+}
+
+func EncryptMasterKeyAES(data []byte, masterKey string) []byte {
+
+	key := GenerateMasterKeyHashSha256(masterKey)
+
+	cipherblock, err := aes.NewCipher(key[:])
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	gcm, err := cipher.NewGCM(cipherblock)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		log.Fatalln(err)
+	}
+	ciphertext := gcm.Seal(nonce, nonce, data, nil)
+
+	return ciphertext
+}
+
+func DecryptAESMasterKey(data []byte, masterkey string) []byte {
+	key := GenerateMasterKeyHashSha256(masterkey)
+
+	block, err := aes.NewCipher(key[:])
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	nonceSize := gcm.NonceSize()
+	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return plaintext
 }
