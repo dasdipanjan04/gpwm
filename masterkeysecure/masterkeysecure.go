603bee44a6bc72899a8414d2d6692b5891d83d0c
Dipanjan Das <dasdipanjan04@gmail.com>
2020-12-19 03:41:16 +0100

    Error ahndling for Encryption and Decryption.

--- masterkeysecure/masterkeysecure.go
+++ masterkeysecure/masterkeysecure.go
@@ -13,39 +13,46 @@ func GenerateMasterKeyHashSha256(password string) [sha256.Size]byte {
 	return sha256.Sum256([]byte(password))
 }
 
-func EncryptMasterKeyAES(data []byte, password string) []byte {
+func EncryptMasterKeyAES(data []byte, password string) ([]byte, error) {
 
 	key := GenerateMasterKeyHashSha256(password)
 
 	cipherblock, err := aes.NewCipher(key[:])
 	if err != nil {
 		glogger.Glog("masterkeysecure:EncryptMasterKeyAES ", err.Error())
+		return nil, err
 	}
 
 	gcm, err := cipher.NewGCM(cipherblock)
 	if err != nil {
 		glogger.Glog("masterkeysecure:EncryptMasterKeyAES ", err.Error())
+		return nil, err
 	}
+
 	nonce := make([]byte, gcm.NonceSize())
 	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
 		glogger.Glog("masterkeysecure:EncryptMasterKeyAES ", err.Error())
+		return nil, err
 	}
+
 	ciphertext := gcm.Seal(nonce, nonce, data, nil)
 
-	return ciphertext
+	return ciphertext, nil
 }
 
-func DecryptAESMasterKey(data []byte, password string) string {
+func DecryptAESMasterKey(data []byte, password string) (string, error) {
 	key := GenerateMasterKeyHashSha256(password)
 
 	cipherblock, err := aes.NewCipher(key[:])
 	if err != nil {
 		glogger.Glog("masterkeysecure:DecryptAESMasterKey ", err.Error())
+		return "", err
 	}
 
 	gcm, err := cipher.NewGCM(cipherblock)
 	if err != nil {
 		glogger.Glog("masterkeysecure:DecryptAESMasterKey ", err.Error())
+		return "", err
 	}
 
 	nonceSize := gcm.NonceSize()
@@ -53,7 +60,8 @@ func DecryptAESMasterKey(data []byte, password string) string {
 	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
 	if err != nil {
 		glogger.Glog("masterkeysecure:DecryptAESMasterKey ", err.Error())
+		return "", err
 	}
 
-	return string(plaintext)
+	return string(plaintext), err
 }
