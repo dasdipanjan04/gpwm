55f29c128f8bf8f373acf51b20cda61d5253d0b3
Dipanjan Das <dasdipanjan04@gmail.com>
2020-12-23 03:37:27 +0100

    [WORKS BUT NOT BEAUTIFUL]: Generate a pdf with QR Coded masterkey

---
+++ helper/gqrpdf/gqrpdf.go
@@ -0,0 +1,59 @@
+package gqrpdf
+
+import (
+	"github.com/dasdipanjan04/gpwm/helper/glogger"
+	"github.com/johnfercher/maroto/pkg/consts"
+	"github.com/johnfercher/maroto/pkg/pdf"
+	"github.com/johnfercher/maroto/pkg/props"
+)
+
+func MasterKeyQRCodePDFGenerator(masterKey string, firstName string, lastname string) {
+
+	qrpdf := pdf.NewMaroto(consts.Portrait, consts.Letter)
+	qrpdf.Row(12, func() {
+		qrpdf.Text(string("Dear Mr./Mrs. "+firstName+" "+lastname),
+			props.Text{
+				Top:         12,
+				Size:        20,
+				Extrapolate: true,
+				Align:       consts.Left,
+				Family:      "arial",
+			})
+	})
+	qrpdf.Row(12, func() {
+		qrpdf.Text("Please scan the QR code below to get your masterkey.",
+			props.Text{
+				Top:         12,
+				Size:        20,
+				Extrapolate: true,
+				Align:       consts.Left,
+				Family:      "arial",
+			})
+	})
+	qrpdf.Row(50, func() {
+		qrpdf.Text("Please save it in a secure place.",
+			props.Text{
+				Top:         12,
+				Size:        20,
+				Extrapolate: true,
+				Align:       consts.Left,
+				Family:      "arial",
+			})
+	})
+	qrpdf.Row(150, func() {
+		qrpdf.Col(12, func() {
+			qrpdf.QrCode(masterKey,
+				props.Rect{
+					Top:     2,
+					Center:  true,
+					Percent: 400,
+				})
+		})
+	})
+
+	err := qrpdf.OutputFileAndClose("gqrpdf.pdf")
+	if err != nil {
+		glogger.Glog("helper:gqrpdf:MasterKeyQRCodePDFGenerator", err.Error())
+	}
+
+}
