b5f1c13354539c107baee9c82139817d630d790c
Dipanjan Das <dasdipanjan04@gmail.com>
2021-04-06 00:30:06 +0200

    1.Remove unnecessary inserts to mastertable. 2.Encrypt app password 3.Insert to passwordmanager table.

--- gpass_main.go
+++ gpass_main.go
@@ -1,26 +1,28 @@
 package main
 
 import (
+	"github.com/dasdipanjan04/gpwm/helper/glogger"
 	mkm "github.com/dasdipanjan04/gpwm/masterpasswordmanager"
+	"github.com/dasdipanjan04/gpwm/passwordmanager"
 )
 
 func main() {
 	db := mkm.CreateMasterKeyTable()
-	mkm.InsertUserDataToDB(db, "TestFN_1", "TestLN_1", "1test@test.test", true)
-	mkm.InsertUserDataToDB(db, "TestFN_2", "TestLN_2", "2test@test.test", true)
-	mkm.InsertUserDataToDB(db, "TestFN_3", "TestLN_3", "3test@test.test", true)
-	mkm.InsertUserDataToDB(db, "TestFN_4", "TestLN_4", "4test@test.test", true)
-	mkm.InsertUserDataToDB(db, "TestFN_5", "TestLN_5", "5test@test.test", true)
-	mkm.InsertUserDataToDB(db, "TestFN_6", "TestLN_6", "6test@test.test", true)
-	mkm.InsertUserDataToDB(db, "TestFN_7", "TestLN_7", "7test@test.test", true)
-	mkm.InsertUserDataToDB(db, "TestFN_8", "TestLN_8", "8test@test.test", true)
-	mkm.InsertUserDataToDB(db, "TestFN_9", "TestLN_9", "9test@test.test", true)
-	mkm.InsertUserDataToDB(db, "TestFN_10", "TestLN_10", "10test@test.test", true)
-	mkm.InsertUserDataToDB(db, "TestFN_11", "TestLN_11", "11test@test.test", true)
-	mkm.InsertUserDataToDB(db, "TestFN_12", "TestLN_12", "12test@test.test", true)
-	mkm.InsertUserDataToDB(db, "TestFN_13", "TestLN_13", "13test@test.test", true)
-	mkm.InsertUserDataToDB(db, "TestFN_14", "TestLN_14", "14test@test.test", true)
-	mkm.InsertUserDataToDB(db, "TestFN_15", "TestLN_15", "15test@test.test", true)
-	mkm.InsertUserDataToDB(db, "TestFN_16", "TestLN_16", "16test@test.test", true)
+	mkm.InsertUserDataToDB(db, "TestFN1", "TestLN1", "1test@test.test", true)
 	mkm.ResetMasterKey(db)
+
+	passwordManagerTable := passwordmanager.CreatePasswordManagerTable()
+	encrypted_app_password, err := passwordmanager.EncryptApplicationPassword(db, passwordManagerTable,
+		"1234",
+		"'1test@test.test'",
+		"facebook",
+		"fbpass")
+	if err != nil {
+		glogger.Glog("Main:EncryptApplicationPassword", err.Error())
+	}
+	passwordmanager.InsertEncryptedPasswordToDB(passwordManagerTable,
+		"facebook",
+		"'1test@test.test'",
+		encrypted_app_password,
+		true)
 }
